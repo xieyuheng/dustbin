22d569885075335cd220ce361fb23af84dba0d58
Xie Yuheng <xyheme@gmail.com>
2023-02-05 01:27:36 +0800

    up

--- fidb-deno/src/database/tests/utils.ts
+++ fidb-deno/src/database/tests/utils.ts
@@ -4,5 +4,5 @@ import { Database } from "../../database/index.ts"
 const filename = new URL(import.meta.url).pathname
 
 export const db = new Database({
-  path: resolve(filename, "../../../../tests/databases/test"),
+  path: resolve(filename, "../../../../tmp/databases/test"),
 })
